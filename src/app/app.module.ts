6c7c9da4c68d448c5a8d4fa63b4499a12f04f588
FERRIE Nicolas <Nicolas.FERRIE@berger-levrault.com>
2022-03-17 13:29:15 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { CocktailListComponent } from './cocktail-list/cocktail-list.component';
+import { CocktailDetailsComponent } from './cocktail-details/cocktail-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    CocktailListComponent,
+    CocktailDetailsComponent
   ],
   imports: [
     BrowserModule,
